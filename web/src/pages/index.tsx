81684f15c3c53055f7df9bde0b0047cd3d0d7e88
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-06 17:35:16 -0300

    feats: create and list products

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { getSession,  } from '@auth0/nextjs-auth0';
+import { getAccessToken, getSession,  } from '@auth0/nextjs-auth0';
 import { GetServerSideProps } from 'next';
 import React from 'react';
 
@@ -13,6 +13,8 @@ const Home: React.FC = () => {
 export default Home;
 
 export const getServerSideProps: GetServerSideProps = async ({req, res}) => {
+  const token = getAccessToken(req, res);
+  console.log(token);
   const session = getSession(req, res);
 
   if (!session) {
