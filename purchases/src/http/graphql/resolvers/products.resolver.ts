73216c4bc05a279cdb785a2b16257c2b653bb843
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-07 10:50:27 -0300

    list purchases

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -7,7 +7,7 @@ import { CreateProductInput } from '../inputs/create-product-input';
 
 import { Product } from '../models/product';
 
-@Resolver('products')
+@Resolver(() => Product)
 export class ProductsResolver {
   constructor(private productsService: ProductsService) {}
 
