bfb717abef91b743dab69becc1e3a9381143ad9d
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-07 12:09:49 -0300

    create purchase & customer profile w/ purchases

--- purchases/src/http/graphql/resolvers/purchases.resolver.ts
+++ purchases/src/http/graphql/resolvers/purchases.resolver.ts
@@ -1,10 +1,19 @@
 import { UseGuards } from '@nestjs/common';
-import { Resolver, Query, ResolveField, Parent } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  ResolveField,
+  Parent,
+  Mutation,
+  Args,
+} from '@nestjs/graphql';
+import { CustomerService } from '../../../services/customers.service';
 import { ProductsService } from '../../../services/products.service';
 
 import { PurchasesService } from '../../../services/purchases.service';
 import { AuthorizationGuard } from '../../auth/authorization.guard';
-import { Product } from '../models/product';
+import { AuthUser, CurrentUser } from '../../auth/current-user';
+import { CreatePurchaseInput } from '../inputs/create-purchase-input';
 
 import { Purchase } from '../models/purchase';
 
@@ -13,6 +22,7 @@ export class PurchasesResolver {
   constructor(
     private purchasesService: PurchasesService,
     private productsService: ProductsService,
+    private customersService: CustomerService,
   ) {}
 
   @Query(() => [Purchase])
@@ -24,5 +34,27 @@ export class PurchasesResolver {
   @ResolveField()
   product(@Parent() purchase: Purchase) {
     return this.productsService.getProductById(purchase.productId);
+  }
+
+  @Mutation(() => Purchase)
+  @UseGuards(AuthorizationGuard)
+  async createPurchase(
+    @Args('data') data: CreatePurchaseInput,
+    @CurrentUser() user: AuthUser,
+  ) {
+    let customer = await this.customersService.getCustomerByAuthUserId(
+      user.sub,
+    );
+
+    if (!customer) {
+      customer = await this.customersService.createCustomer({
+        authUserId: user.sub,
+      });
+    }
+
+    return this.purchasesService.createPurchase({
+      customerId: customer.id,
+      productId: data.productId,
+    });
   }
 }
