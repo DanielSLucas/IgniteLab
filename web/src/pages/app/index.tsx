8cac83d2739fc5d1554dda0ec8eef55c03ec1cfe
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-11 16:18:36 -0300

    graphql & apolloClient config

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,12 +1,22 @@
 import { withPageAuthRequired, useUser } from '@auth0/nextjs-auth0';
 import React from 'react';
+import { useMeQuery } from '../../graphql/generated/graphql';
+import { getServerPageGetProducts, ssrGetProducts } from '../../graphql/generated/page';
+import { withApollo } from '../../lib/withApollo';
 
-const Home: React.FC = () => {
+const Home: React.FC = ({ data }: any) => {
   const { user } = useUser();
+  const { data: me} = useMeQuery();
   
   return (
-    <div>
+    <div className='text-violet-500'>
       <h1>Hello world!</h1>
+      <pre>
+        {JSON.stringify(me, null, 2)}
+      </pre>
+      {/* <pre>
+        {JSON.stringify(data.products, null, 2)}
+      </pre> */}
       <pre>
         {JSON.stringify(user, null, 2)}
       </pre>      
@@ -14,6 +24,15 @@ const Home: React.FC = () => {
   );
 }
 
-export default Home;
+export const getServerSideProps = withPageAuthRequired({
+  getServerSideProps: async (ctx) => {    
+    // return getServerPageGetProducts({}, ctx);
+    return{
+      props: {}
+    };
+  }
+});
 
-export const getServerSideProps = withPageAuthRequired();
+export default withApollo(
+  ssrGetProducts.withPage()(Home)
+);
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,20 +1,14 @@
-import { getAccessToken, getSession,  } from '@auth0/nextjs-auth0';
+import { getSession } from '@auth0/nextjs-auth0';
 import { GetServerSideProps } from 'next';
 import React from 'react';
 
 const Home: React.FC = () => {
-  
-  
-  
-
   return null;
 }
 
 export default Home;
 
 export const getServerSideProps: GetServerSideProps = async ({req, res}) => {
-  const token = getAccessToken(req, res);
-  console.log(token);
   const session = getSession(req, res);
 
   if (!session) {
